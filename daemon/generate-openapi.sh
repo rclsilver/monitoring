0f0768cf6c0cb444d83ac9a063d38b5c3ba51f54
Thomas Bétrancourt <thomas@betrancourt.net>
2024-04-11 07:48:14 +0200

    wip: mqtt daemon build

--- daemon/generate-openapi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -eux
-
-curl --fail -o ./frontend/src/backend/api/openapi.json ${SERVER_URL:-http://localhost:8080}/api/spec.json
-
-docker-compose run --rm -u node -w /app frontend npm run openapi
